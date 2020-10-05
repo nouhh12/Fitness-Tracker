fac5e221d619afade8fd737491da9af0946d7aea
Mohamed Nouh <nouhonenote@hotmail.com>
2020-10-05 18:02:08 +0000

    Update FitnessTrack.java

--- src/tracker/FitnessTrack.java
+++ src/tracker/FitnessTrack.java
@@ -195,6 +195,7 @@ public class FitnessTrack extends JFrame {
 					mytimer.setRepeats(false);
 				}
 				else {
+				//To pass the time of the activity to Track object
 				int time=Integer.parseInt(insertBox.getText());
 				track.getCaloriesBurnt(time, "Kick_Boxing");
 				track.heartRateIncrease(time, "Kick_Boxing");
